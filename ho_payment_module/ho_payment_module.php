e189b4af156d303c4e83a133806d7db60ee548c0
Xurxo Freitas <152525151+Xurxo8@users.noreply.github.com>
2025-10-21 09:46:58 +0200

    Estructura JSON para almacenar datos
    Ya se pueden almacenar los datos de los métodos de pago en una variable de seguridad con una estructura en JSON

--- ho_payment_module/ho_payment_module.php
+++ ho_payment_module/ho_payment_module.php
@@ -2,225 +2,250 @@
 /**
 * 2007-2025 PrestaShop
 *
-* NOTICE OF LICENSE
+* AVISO DE LICENCIA
 *
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
+* Este archivo fuente está sujeto a la Licencia Académica Libre (AFL 3.0)
+* que se incluye con este paquete en el archivo LICENSE.txt.
+* También está disponible a través de la web en la siguiente URL:
 * http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
+* Si no ha recibido una copia de la licencia y no puede
+* obtenerla a través de Internet, envíe un correo electrónico a
+* license@prestashop.com para que podamos enviársela de inmediato.
 *
-* DISCLAIMER
+* DESCARGO DE RESPONSABILIDAD
 *
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
+* No edite ni agregue a este archivo si desea actualizar PrestaShop a versiones más recientes.
+* Si desea personalizar PrestaShop según sus necesidades,
+* consulte http://www.prestashop.com para obtener más información.
 *
 *  @author    PrestaShop SA <contact@prestashop.com>
 *  @copyright 2007-2025 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Licencia Académica Libre (AFL 3.0)
+*  Marca registrada internacional y propiedad de PrestaShop SA
 */
 
 if (!defined('_PS_VERSION_')) {
-    exit;
+  exit;
 }
 
 class Ho_payment_module extends Module
 {
-    protected $config_form = false;
+  protected $config_form = false;
 
-    public function __construct()
-    {
-        $this->name = 'ho_payment_module';
-        $this->tab = 'administration';
-        $this->version = '1.0.0';
-        $this->author = 'Xurxo';
-        $this->need_instance = 0;
-
-        /**
-         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
-         */
-        $this->bootstrap = true;
-
-        parent::__construct();
-
-        $this->displayName = $this->l('hoPaimentModule');
-        $this->description = $this->l('Modulo donde se mostrarán las formas de pago');
-
-        $this->confirmUninstall = $this->l('¿Seguro que quieres desinstalar?');
-
-        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => '9.0');
-    }
+  public function __construct()
+  {
+    $this->name = 'ho_payment_module';
+    $this->tab = 'administration';
+    $this->version = '1.0.0';
+    $this->author = 'Xurxo';
+    $this->need_instance = 0;
 
     /**
-     * Don't forget to create update methods if needed:
-     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     * Establece $this->bootstrap en true si tu módulo es compatible con bootstrap (PrestaShop 1.6)
      */
-    public function install()
-    {
-        Configuration::updateValue('HO_PAYMENT_MODULE_LIVE_MODE', false);
+    $this->bootstrap = true;
 
-        return parent::install() &&
-            $this->registerHook('header') &&
-            $this->registerHook('displayBackOfficeHeader') &&
-            $this->registerHook('displayFooter');
-    }
+    parent::__construct();
 
-    public function uninstall()
-    {
-        Configuration::deleteByName('HO_PAYMENT_MODULE_LIVE_MODE');
+    $this->displayName = $this->l('hoPaimentModule');
+    $this->description = $this->l('Módulo donde se mostrarán las formas de pago');
 
-        return parent::uninstall();
-    }
+    $this->confirmUninstall = $this->l('¿Seguro que quieres desinstalar?');
 
+    $this->ps_versions_compliancy = array('min' => '1.6', 'max' => '9.0');
+  }
+
+  /**
+   * No olvides crear los métodos de actualización si es necesario:
+   * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+   */
+  public function install(){
+    Configuration::updateValue('HO_PAYMENT_MODULE_LIVE_MODE', false);
+
+    return parent::install() &&
+      $this->registerHook('header') &&
+      $this->registerHook('displayBackOfficeHeader') &&
+      $this->registerHook('displayFooter');
+  }
+
+  public function uninstall(){
+    Configuration::deleteByName('HO_PAYMENT_MODULE_LIVE_MODE');
+
+    return parent::uninstall();
+  }
+
+  /**
+   * Carga el formulario de configuración
+   */
+  public function getContent(){
     /**
-     * Load the configuration form
+     * Si se han enviado valores en el formulario, los procesa.
      */
-    public function getContent()
-    {
-        /**
-         * If values have been submitted in the form, process.
-         */
-        if (((bool)Tools::isSubmit('submitHo_payment_moduleModule')) == true) {
-            $this->postProcess();
+    if (((bool)Tools::isSubmit('submitHo_payment_moduleModule')) == true) {
+      $this->postProcess();
+    }
+
+    $this->context->smarty->assign('module_dir', $this->_path);
+
+    $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+
+    return $output.$this->renderForm();
+  }
+
+  /**
+   * Crea el formulario que se mostrará en la configuración del módulo.
+   */
+  protected function renderForm(){
+    $helper = new HelperForm();
+
+    $helper->show_toolbar = false;
+    $helper->table = $this->table;
+    $helper->module = $this;
+    $helper->default_form_language = $this->context->language->id;
+    $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+    $helper->identifier = $this->identifier;
+    $helper->submit_action = 'submitHo_payment_moduleModule';
+    $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+      .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+    $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+    $helper->tpl_vars = array(
+      'fields_value' => $this->getConfigFormValues(), /* Añade valores para los campos del formulario */
+      'languages' => $this->context->controller->getLanguages(),
+      'id_language' => $this->context->language->id,
+    );
+
+    return $helper->generateForm(array($this->getConfigForm()));
+  }
+
+  /**
+   * Crea la estructura del formulario.
+   */
+  protected function getConfigForm(){
+    return array(
+      'form' => array(
+        'legend' => array(
+          'title' => $this->l('Configuración de los métodos de pago'),
+          'icon' => 'icon-credit-card',
+        ),
+        'input' => array(
+          array(
+            'col' => 4,
+            'type' => 'text',
+            'label' => $this->l('Nombre: '),
+            'name' => 'HO_PAYMENT_MODULE_NAME',
+            'reqiured' => true,
+            'desc' => $this->l('Introduce el nombre que se mostrará al cliente (por ejemplo: “PayPal” o “Bizum”).'),
+          ),
+          array(
+            'type' => 'file',
+            'label' => $this->l('Logo: '),            
+            'name' => 'HO_PAYMENT_MODULE_LOGO',
+            'desc' => $this->l('Sube una imagen para el logo del método de pago (formato: SVG o WEBP).'),
+            'display_image' => true,
+            'image' => (Configuration::get('HO_PAYMENT_MODULE_LOGO'))
+            ? '<img src="'.$this->_path.'views/img/'.Configuration::get('HO_PAYMENT_MODULE_LOGO').'" height="80" />'
+            : '',
+          ),
+        ),
+        'submit' => array(
+          'title' => $this->l('Guardar'),
+        ),
+      ),
+    );
+  }
+
+  /**
+   * Define los valores de los campos del formulario.
+   */
+  protected function getConfigFormValues(){
+    return array(
+    'HO_PAYMENT_MODULE_NAME' => Configuration::get('HO_PAYMENT_MODULE_NAME', ''),
+    'HO_PAYMENT_MODULE_LOGO' => Configuration::get('HO_PAYMENT_MODULE_LOGO', ''),
+  );
+  }
+
+  /**
+   * Guarda los datos del formulario.
+   */
+  protected function postProcess(){
+    $errores = [];
+    $nombre = Tools::getValue('HO_PAYMENT_MODULE_NAME');
+
+    // Validar nombre
+    if (empty($nombre) || !Validate::isGenericName($nombre)) {
+      $errors[] = $this->l('El nombre del método de pago no es válido o está vacío.');
+    }
+
+    // Procesar logo si se sube
+    $logoNombre = '';
+    if (isset($_FILES['HO_PAYMENT_MODULE_LOGO']) && !empty($_FILES['HO_PAYMENT_MODULE_LOGO']['tmp_name'])) {
+      $logo = $_FILES['HO_PAYMENT_MODULE_LOGO'];
+      $tipoImgValido = ['image/svg+xml', 'image/webp'];
+
+      if (!in_array($logo['type'], $tipoImgValido)) {
+        $errors[] = $this->l('Formato de imagen no permitido. Usa SVG o WEBP.');
+      } else {
+        $path = $this->local_path . 'views/img/';
+        if (!file_exists($path)) {
+          mkdir($path, 0755, true);
         }
 
-        $this->context->smarty->assign('module_dir', $this->_path);
-
-        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
-
-        return $output.$this->renderForm();
-    }
-
-    /**
-     * Create the form that will be displayed in the configuration of your module.
-     */
-    protected function renderForm()
-    {
-        $helper = new HelperForm();
-
-        $helper->show_toolbar = false;
-        $helper->table = $this->table;
-        $helper->module = $this;
-        $helper->default_form_language = $this->context->language->id;
-        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
-
-        $helper->identifier = $this->identifier;
-        $helper->submit_action = 'submitHo_payment_moduleModule';
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
-            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
-        $helper->token = Tools::getAdminTokenLite('AdminModules');
-
-        $helper->tpl_vars = array(
-            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
-            'languages' => $this->context->controller->getLanguages(),
-            'id_language' => $this->context->language->id,
-        );
-
-        return $helper->generateForm(array($this->getConfigForm()));
-    }
-
-    /**
-     * Create the structure of your form.
-     */
-    protected function getConfigForm()
-    {
-        return array(
-            'form' => array(
-                'legend' => array(
-                'title' => $this->l('Settings'),
-                'icon' => 'icon-cogs',
-                ),
-                'input' => array(
-                    array(
-                        'type' => 'switch',
-                        'label' => $this->l('Live mode'),
-                        'name' => 'HO_PAYMENT_MODULE_LIVE_MODE',
-                        'is_bool' => true,
-                        'desc' => $this->l('Use this module in live mode'),
-                        'values' => array(
-                            array(
-                                'id' => 'active_on',
-                                'value' => true,
-                                'label' => $this->l('Enabled')
-                            ),
-                            array(
-                                'id' => 'active_off',
-                                'value' => false,
-                                'label' => $this->l('Disabled')
-                            )
-                        ),
-                    ),
-                    array(
-                        'col' => 3,
-                        'type' => 'text',
-                        'prefix' => '<i class="icon icon-envelope"></i>',
-                        'desc' => $this->l('Enter a valid email address'),
-                        'name' => 'HO_PAYMENT_MODULE_ACCOUNT_EMAIL',
-                        'label' => $this->l('Email'),
-                    ),
-                    array(
-                        'type' => 'password',
-                        'name' => 'HO_PAYMENT_MODULE_ACCOUNT_PASSWORD',
-                        'label' => $this->l('Password'),
-                    ),
-                ),
-                'submit' => array(
-                    'title' => $this->l('Save'),
-                ),
-            ),
-        );
-    }
-
-    /**
-     * Set values for the inputs.
-     */
-    protected function getConfigFormValues()
-    {
-        return array(
-            'HO_PAYMENT_MODULE_LIVE_MODE' => Configuration::get('HO_PAYMENT_MODULE_LIVE_MODE', true),
-            'HO_PAYMENT_MODULE_ACCOUNT_EMAIL' => Configuration::get('HO_PAYMENT_MODULE_ACCOUNT_EMAIL', 'contact@prestashop.com'),
-            'HO_PAYMENT_MODULE_ACCOUNT_PASSWORD' => Configuration::get('HO_PAYMENT_MODULE_ACCOUNT_PASSWORD', null),
-        );
-    }
-
-    /**
-     * Save form data.
-     */
-    protected function postProcess()
-    {
-        $form_values = $this->getConfigFormValues();
-
-        foreach (array_keys($form_values) as $key) {
-            Configuration::updateValue($key, Tools::getValue($key));
+        $logoNombre = 'logo_' . time() . '.' . pathinfo($logo['name'], PATHINFO_EXTENSION);
+        if (!move_uploaded_file($logo['tmp_name'], $path . $logoNombre)) {
+          $errors[] = $this->l('No se pudo subir el archivo del logo.');          
         }
+      }
     }
 
-    /**
-    * Add the CSS & JavaScript files you want to be loaded in the BO.
-    */
-    public function hookDisplayBackOfficeHeader()
-    {
-        if (Tools::getValue('configure') == $this->name) {
-            $this->context->controller->addJS($this->_path.'views/js/back.js');
-            $this->context->controller->addCSS($this->_path.'views/css/back.css');
-        }
-    }
+    // Guardar en JSON si no hay errores
+    if (empty($errors)) {
+      // Recuperar métodos existentes
+      $metodosJSON = Configuration::get('HO_PAYMENT_MODULE_PAYMENTS');
+      $metodos = $metodosJSON ? json_decode($metodosJSON, true) : [];
 
-    /**
-     * Add the CSS & JavaScript files you want to be added on the FO.
-     */
-    public function hookHeader()
-    {
-        $this->context->controller->addJS($this->_path.'/views/js/front.js');
-        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
-    }
+      // Generar un nuevo ID
+      $nuveoId = count($metodos) ? max(array_column($metodos, 'id')) + 1 : 1;
 
-    public function hookDisplayFooter()
-    {
-        /* Place your code here. */
+      // Crear nuevo metodo de pago
+      $nuevoMetodo = [
+        'id' => $nuevoID,
+        'nombre' => $nombre,
+        'logo' => $logoNombre
+      ];
+
+      $metodos[] = $nuevoMetodo;
+
+      Configuration::updateValue('HO_PAYMENT_MODULE_NAME', $nombre);
+      return $this->displayConfirmation($this->l('Configuración guardada correctamente.'));
+    } else {
+      return $this->displayError(implode('<br>', $errors));
     }
+  }
+
+  /**
+  * Añade los archivos CSS y JavaScript que se cargarán en el panel de administración.
+  */
+  public function hookDisplayBackOfficeHeader()
+  {
+    if (Tools::getValue('configure') == $this->name) {
+      $this->context->controller->addJS($this->_path.'views/js/back.js');
+      $this->context->controller->addCSS($this->_path.'views/css/back.css');
+    }
+  }
+
+  /**
+   * Añade los archivos CSS y JavaScript que se cargarán en el front-office.
+   */
+  public function hookHeader()
+  {
+    $this->context->controller->addJS($this->_path.'/views/js/front.js');
+    $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+  }
+
+  public function hookDisplayFooter()
+  {
+    /* Inserta aquí tu código. */
+  }
 }
